cc269924e8a024b96ecd32f5277bbc7b47c0a6c7
Tim Birkert <tim.birkert@gmail.com>
2025-05-29 13:27:37 +0200

    Update and rename Abbonennten.py to functions/Abbonennten.py

---
+++ functions/Abbonennten.py
@@ -0,0 +1,23 @@
+#dictionary that contains all recipients of your newsletter
+
+Abos={
+    "Abonennt1": {
+        "Ort": "Wohnort1",
+        "Geburtstag": "25.09.2002",
+        "Mail": "vorname.nachname@gmail.com"
+    },
+    "Abonennt2": {
+        "Ort": "Wohnort2",
+        "Geburtstag": "25.09.2002",
+        "Mail": "vorname.nachname@gmail.com"
+    },
+     "Abonennt3": {
+        "Ort": "Wohnort1",
+        "Geburtstag": "25.09.2002",
+        "Mail": "vorname.nachname@gmail.com"
+    },
+   "Abonennt1": {
+        "Ort": "Wohnort1",
+        "Geburtstag": "25.09.2002",
+        "Mail": "vorname.nachname@gmail.com"
+    }}
